a12185ab9f1c68c70ac08b341ca8b134a5b16486
Robert Arnesson <robert@robertarnesson.com>
2016-06-13 20:45:38 -0400

    js fix

--- www/firebase.js
+++ www/firebase.js
@@ -1,7 +1,5 @@
-cordova.define("cordova-plugin-firebase.FirebasePlugin", function(require, exports, module) {
-	var exec = require('cordova/exec');
+var exec = require('cordova/exec');
 
-	exports.getRegistrationId = function(success, error) {
-	    exec(success, error, "FirebasePlugin", "getRegistrationId", []);
-	};
-});
+exports.getRegistrationId = function(success, error) {
+    exec(success, error, "FirebasePlugin", "getRegistrationId", []);
+};
