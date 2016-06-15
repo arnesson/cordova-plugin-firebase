fea6f065a356292008eb8fd8edf3575d79a96d3f
Robert Arnesson <robert@robertarnesson.com>
2016-06-15 16:26:14 -0400

    initial android FCM implementation

--- src/android/FirebasePlugin.java
+++ src/android/FirebasePlugin.java
@@ -1,5 +1,7 @@
 package org.apache.cordova.firebase;
 
+import android.util.Log;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -7,9 +9,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * This class echoes a string called from JavaScript.
- */
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
 public class FirebasePlugin extends CordovaPlugin {
 
     @Override
@@ -22,6 +28,46 @@ public class FirebasePlugin extends CordovaPlugin {
     }
 
     private void getRegistrationId(CallbackContext callbackContext) {
-        callbackContext.success("NOT IMPLEMENTED");
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        callbackContext.success(refreshedToken);
+    }
+}
+
+
+public class FirebasePluginInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "FirebasePluginInstanceIDService";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    }
+}
+
+
+public class FirebasePluginMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FirebasePluginMessagingService";
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
     }
 }
