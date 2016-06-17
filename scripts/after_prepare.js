f0863046cc796a33b734dd40f06ca1e2c83031f5
Robert Arnesson <robert@robertarnesson.com>
2016-06-16 22:48:35 -0400

    GoogleService-Info.plist generation by hook

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -23,8 +23,64 @@ var getValue = function(config, name) {
 
 var config = fs.readFileSync("config.xml").toString()
 var name = getValue(config, "name")
+var AD_UNIT_ID_FOR_BANNER_TEST = getPreferenceValue(config, "AD_UNIT_ID_FOR_BANNER_TEST")
+var AD_UNIT_ID_FOR_INTERSTITIAL_TEST = getPreferenceValue(config, "AD_UNIT_ID_FOR_INTERSTITIAL_TEST")
+var CLIENT_ID = getPreferenceValue(config, "CLIENT_ID")
+var REVERSED_CLIENT_ID = getPreferenceValue(config, "REVERSED_CLIENT_ID")
+var API_KEY = getPreferenceValue(config, "API_KEY")
+var GCM_SENDER_ID = getPreferenceValue(config, "GCM_SENDER_ID")
+var BUNDLE_ID = getPreferenceValue(config, "BUNDLE_ID")
+var PROJECT_ID = getPreferenceValue(config, "PROJECT_ID")
+var STORAGE_BUCKET = getPreferenceValue(config, "STORAGE_BUCKET")
+var IS_ADS_ENABLED = getPreferenceValue(config, "IS_ADS_ENABLED")
+var IS_ANALYTICS_ENABLED = getPreferenceValue(config, "IS_ANALYTICS_ENABLED")
+var IS_APPINVITE_ENABLED = getPreferenceValue(config, "IS_APPINVITE_ENABLED")
+var IS_GCM_ENABLED = getPreferenceValue(config, "IS_GCM_ENABLED")
+var IS_SIGNIN_ENABLED = getPreferenceValue(config, "IS_SIGNIN_ENABLED")
+var GOOGLE_APP_ID = getPreferenceValue(config, "GOOGLE_APP_ID")
+var DATABASE_URL = getPreferenceValue(config, "DATABASE_URL")
 
 try {
-	var contents = fs.readFileSync("ios/GoogleService-Info.plist").toString()
+    var contents = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+"<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n" +
+"<plist version=\"1.0\">\n" +
+"<dict>\n" +
+"    <key>AD_UNIT_ID_FOR_BANNER_TEST</key>\n" +
+"    <string>" + AD_UNIT_ID_FOR_BANNER_TEST + "</string>\n" +
+"    <key>AD_UNIT_ID_FOR_INTERSTITIAL_TEST</key>\n" +
+"    <string>" + AD_UNIT_ID_FOR_INTERSTITIAL_TEST + "</string>\n" +
+"    <key>CLIENT_ID</key>\n" +
+"    <string>" + CLIENT_ID + "</string>\n" +
+"    <key>REVERSED_CLIENT_ID</key>\n" +
+"    <string>" + REVERSED_CLIENT_ID + "</string>\n" +
+"    <key>API_KEY</key>\n" +
+"    <string>" + API_KEY + "</string>\n" +
+"    <key>GCM_SENDER_ID</key>\n" +
+"    <string>" + GCM_SENDER_ID + "</string>\n" +
+"    <key>PLIST_VERSION</key>\n" +
+"    <string>1</string>\n" +
+"    <key>BUNDLE_ID</key>\n" +
+"    <string>" + BUNDLE_ID + "</string>\n" +
+"    <key>PROJECT_ID</key>\n" +
+"    <string>" + PROJECT_ID + "</string>\n" +
+"    <key>STORAGE_BUCKET</key>\n" +
+"    <string>" + STORAGE_BUCKET + "</string>\n" +
+"    <key>IS_ADS_ENABLED</key>\n" +
+"    <" + IS_ADS_ENABLED + "/>\n" +
+"    <key>IS_ANALYTICS_ENABLED</key>\n" +
+"    <" + IS_ANALYTICS_ENABLED + "/>\n" +
+"    <key>IS_APPINVITE_ENABLED</key>\n" +
+"    <" + IS_APPINVITE_ENABLED + "/>\n" +
+"    <key>IS_GCM_ENABLED</key>\n" +
+"    <" + IS_GCM_ENABLED + "/>\n" +
+"    <key>IS_SIGNIN_ENABLED</key>\n" +
+"    <" + IS_SIGNIN_ENABLED + "/>\n" +
+"    <key>GOOGLE_APP_ID</key>\n" +
+"    <string>" + GOOGLE_APP_ID + "</string>\n" +
+"    <key>DATABASE_URL</key>\n" +
+"    <string>" + DATABASE_URL + "</string>\n" +
+"</dict>\n" +
+"</plist>\n"
+
     fs.writeFileSync("platforms/ios/" + name + "/GoogleService-Info.plist", contents)
 } catch(err) {}
