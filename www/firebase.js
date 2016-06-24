139ee0871a7ed9b91603f353b8cb67b38bb8dbd7
Chuckv01 <charles.vanderhoff@gmail.com>
2016-06-24 06:08:05 -0700

    Update firebase.js

--- www/firebase.js
+++ www/firebase.js
@@ -4,6 +4,10 @@ exports.getRegistrationId = function(success, error) {
     exec(success, error, "FirebasePlugin", "getRegistrationId", []);
 };
 
-exports.logEvent = function(category, action, label, success, error) {
+exports.startAnalytics = function(success, error) {
+    exec(success, error, "FirebasePlugin", "startAnalytics", []);
+};
+
+exports.logEvent = function(category, action, success, error) {
     exec(success, error, "FirebasePlugin", "logEvent", [category, action]);
 };
