c4e7c60439e89979b639e4e1236fe3894787ae04
Chuckv01 <charles.vanderhoff@gmail.com>
2016-06-23 20:00:01 -0700

    analytics initial commit

--- src/android/FirebasePlugin.java
+++ src/android/FirebasePlugin.java
@@ -10,14 +10,19 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 
 public class FirebasePlugin extends CordovaPlugin {
-
+    private FirebaseAnalytics mFirebaseAnalytics;
+    
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("getRegistrationId")) {
             this.getRegistrationId(callbackContext);
+            return true;
+        } else if (action.equals("startAnalytics")) {
+            this.startTracker(callbackContext);
             return true;
         }
         return false;
@@ -26,5 +31,14 @@ public class FirebasePlugin extends CordovaPlugin {
     private void getRegistrationId(CallbackContext callbackContext) {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         callbackContext.success(refreshedToken);
+    }
+    
+    private void startTracker(CallbackContext callbackContext) {
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        if (mFirebaseAnalytics) {
+            callbackContext.success("tracker started");
+        } else {
+            callbackContext.error("tracker not started");
+        }
     }
 }
