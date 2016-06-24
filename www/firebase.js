35e3682f87f17bb15ce9d3287ece3aabe1433c80
Chuckv01 <charles.vanderhoff@gmail.com>
2016-06-24 06:01:56 -0700

    Update firebase.js

--- www/firebase.js
+++ www/firebase.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.getRegistrationId = function(success, error) {
     exec(success, error, "FirebasePlugin", "getRegistrationId", []);
 };
+
+exports.logEvent = function(category, action, label, success, error) {
+    exec(success, error, "FirebasePlugin", "logEvent", [category, action]);
+};
