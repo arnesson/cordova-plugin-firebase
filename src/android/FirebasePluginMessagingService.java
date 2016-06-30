13fe41e8ecd80c65a1a7011f809670279164d1bf
Júlio Cesar Bueno Cotta <juliocbcotta@gmail.com>
2016-06-29 22:32:01 -0300

    adds broadcast receiver to listen notification open

--- src/android/FirebasePluginMessagingService.java
+++ src/android/FirebasePluginMessagingService.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -57,15 +58,14 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
     }
 
     private void sendNotification(int id, String title, String messageBody, Map<String, String> data) {
-        PackageManager pm = getPackageManager();
-        Intent intent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());
-
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        Intent intent = new Intent(this, OnNotificationOpenReceiver.class);
+        Bundle bundle = new Bundle();
         for (String key : data.keySet()) {
-            intent.putExtra(key, data.get(key));
+            bundle.putString(key, data.get(key));
         }
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
+        intent.putExtras(bundle);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
