806af2be91bb0bb448555af3aab3492b86244541
Robert Arnesson <robert@robertarnesson.com>
2016-08-26 15:33:59 -0400

    look for config files in root or www to support phonegap build

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -33,40 +33,56 @@ function directoryExists(path) {
 var config = fs.readFileSync("config.xml").toString()
 var name = getValue(config, "name")
 
-if(fileExists("GoogleService-Info.plist") && directoryExists("platforms/ios/")){
-  try {
-  	var contents = fs.readFileSync("GoogleService-Info.plist").toString();
-      fs.writeFileSync("platforms/ios/" + name + "/Resources/GoogleService-Info.plist", contents)
-  } catch(err) {
-    process.stdout.write(err);
+if (directoryExists("platforms/ios")) {
+  var paths = ["GoogleService-Info.plist", "platforms/ios/www/GoogleService-Info.plist"];
+
+  for (var i = 0; i < paths.length; i++) {
+    if (fileExists(paths[i])) {
+      try {
+        var contents = fs.readFileSync(paths[i]).toString();
+        fs.writeFileSync("platforms/ios/" + name + "/Resources/GoogleService-Info.plist", contents)
+      } catch(err) {
+        process.stdout.write(err);
+      }
+
+      break;
+    }
   }
 }
 
-if(fileExists("google-services.json") && directoryExists("platforms/android/")){
-	try {
-		var contents = fs.readFileSync("google-services.json").toString();
-	    fs.writeFileSync("platforms/android/google-services.json", contents);
-	
-	    var json = JSON.parse(contents);
-	    var strings = fs.readFileSync("platforms/android/res/values/strings.xml").toString();
-	
-	    // strip non-default value
-	    strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)</string>', "i"), '')
-	
-	    // strip non-default value
-	    strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)</string>', "i"), '')
-	
-	    // strip empty lines
-	    strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1')
-	
-	    // replace the default value
-	    strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)</string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>')
-	
-	    // replace the default value
-	    strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>')
-	
-	    fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
-	} catch(err) {
-	    process.stdout.write(err);
-	}
+if (directoryExists("platforms/android")) {
+  var paths = ["google-services.json", "platforms/android/assets/www/google-services.json"];
+
+  for (var i = 0; i < paths.length; i++) {
+    if (fileExists(paths[i])) {
+      try {
+        var contents = fs.readFileSync(paths[i]).toString();
+        fs.writeFileSync("platforms/android/google-services.json", contents);
+
+        var json = JSON.parse(contents);
+        var strings = fs.readFileSync("platforms/android/res/values/strings.xml").toString();
+
+        // strip non-default value
+        strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)</string>', "i"), '')
+
+        // strip non-default value
+        strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)</string>', "i"), '')
+
+        // strip empty lines
+        strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1')
+
+        // replace the default value
+        strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)</string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>')
+
+        // replace the default value
+        strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>')
+
+        fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
+      } catch(err) {
+        process.stdout.write(err);
+      }
+
+      break;
+    }
+  }
 }
