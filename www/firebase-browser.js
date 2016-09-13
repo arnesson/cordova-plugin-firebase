425129fe54ff05eee544d2bd3c4be66eb963a2d2
Robert Arnesson <robert@robertarnesson.com>
2016-09-13 15:00:37 -0400

    added proper browser proxy

---
+++ www/firebase-browser.js
@@ -0,0 +1,67 @@
+exports.getInstanceId = function(success, error) {
+    success();
+};
+
+exports.onNotificationOpen = function(success, error) {
+    success();
+};
+
+exports.grantPermission = function(success, error) {
+    success();
+};
+
+exports.setBadgeNumber = function(number, success, error) {
+    success();
+};
+
+exports.getBadgeNumber = function(success, error) {
+    success();
+};
+
+exports.subscribe = function(topic, success, error) {
+    success();
+};
+
+exports.unsubscribe = function(topic, success, error) {
+    success();
+};
+
+exports.logEvent = function(name, params, success, error) {
+    success();
+};
+
+exports.setUserId = function(id, success, error) {
+    success();
+};
+
+exports.setUserProperty = function(name, value, success, error) {
+    success();
+};
+
+exports.activateFetched = function (success, error) {
+    success();
+};
+
+exports.fetch = function (cacheExpirationSeconds, success, error) {
+    success();
+};
+
+exports.getByteArray = function (key, namespace, success, error) {
+    success();
+};
+
+exports.getValue = function (key, namespace, success, error) {
+    success();
+};
+
+exports.getInfo = function (success, error) {
+    success();
+};
+
+exports.setConfigSettings = function (settings, success, error) {
+    success();
+};
+
+exports.setDefaults = function (defaults, namespace, success, error) {
+    success();
+};
