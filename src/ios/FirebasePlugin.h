de05c0e568bb2d4c40b6ed378d1296b3c627ec3b
achuinard <tony.chuinard@gmail.com>
2016-09-22 13:08:34 -0500

    Adding token refresh callback to make plugin more usable

--- src/ios/FirebasePlugin.h
+++ src/ios/FirebasePlugin.h
@@ -10,12 +10,15 @@
 - (void)subscribe:(CDVInvokedUrlCommand*)command;
 - (void)unsubscribe:(CDVInvokedUrlCommand*)command;
 - (void)onNotificationOpen:(CDVInvokedUrlCommand*)command;
+- (void)onTokenRefreshNotification:(CDVInvokedUrlCommand*)command;
 - (void)sendNotification:(NSDictionary*)userInfo;
+- (void)tokenRefreshNotification:(NSString*)token;
 - (void)logEvent:(CDVInvokedUrlCommand*)command;
 - (void)setUserId:(CDVInvokedUrlCommand*)command;
 - (void)setUserProperty:(CDVInvokedUrlCommand*)command;
 
 @property (nonatomic, copy) NSString *notificationCallbackId;
+@property (nonatomic, copy) NSString *tokenRefreshCallbackId;
 @property (nonatomic, retain) NSMutableArray *notificationStack;
 
 @end
