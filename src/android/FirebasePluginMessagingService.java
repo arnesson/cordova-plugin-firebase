cc7f6cfdc64fdcf39c861ec50e701145bc918bd9
Júlio Cesar Bueno Cotta <juliocbcotta@gmail.com>
2016-09-22 00:44:25 -0300

    better handling of open notification on android

--- src/android/FirebasePluginMessagingService.java
+++ src/android/FirebasePluginMessagingService.java
@@ -29,43 +29,50 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
      */
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        // [START_EXCLUDE]
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        // [END_EXCLUDE]
+
         // TODO(developer): Handle FCM messages here.
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-        String title = null;
-        String text = null;
-        int id = 0;
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        String title;
+        String text;
+        String id;
         if (remoteMessage.getNotification() != null) {
             title = remoteMessage.getNotification().getTitle();
             text = remoteMessage.getNotification().getBody();
+            id = remoteMessage.getMessageId();
         } else {
             title = remoteMessage.getData().get("title");
             text = remoteMessage.getData().get("text");
-            try {
-                id = Integer.valueOf(remoteMessage.getData().get("id"));
-            } catch (Exception e) {
-                // ignore
-            }
+            id = remoteMessage.getData().get("id");
+
         }
         Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message id: " + id);
         Log.d(TAG, "Notification Message Title: " + title);
         Log.d(TAG, "Notification Message Body/Text: " + text);
 
         // TODO: Add option to developer to configure if show notification when app on foreground
         if (!TextUtils.isEmpty(text) || !TextUtils.isEmpty(title)) {
             sendNotification(id, title, text, remoteMessage.getData());
-         }
+        }
     }
 
-    private void sendNotification(int id, String title, String messageBody, Map<String, String> data) {
+    private void sendNotification(String id, String title, String messageBody, Map<String, String> data) {
         Intent intent = new Intent(this, OnNotificationOpenReceiver.class);
         Bundle bundle = new Bundle();
         for (String key : data.keySet()) {
             bundle.putString(key, data.get(key));
         }
         intent.putExtras(bundle);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent,
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, id.hashCode(), intent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
@@ -81,7 +88,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(id, notificationBuilder.build());
+        notificationManager.notify(id.hashCode(), notificationBuilder.build());
     }
 
 
