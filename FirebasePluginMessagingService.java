0be4970e705260c7ca2c4acec253451e9c5ccd58
Luis <l.zapatagt@gmail.com>
2016-10-23 20:55:20 -0400

    Notification Icon
    Update to make easier the notification's icon setup

---
+++ FirebasePluginMessagingService.java
@@ -0,0 +1,99 @@
+package org.apache.cordova.firebase;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.text.TextUtils;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+
+public class FirebasePluginMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FirebasePlugin";
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // [START_EXCLUDE]
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        // [END_EXCLUDE]
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        String title;
+        String text;
+        String id;
+        if (remoteMessage.getNotification() != null) {
+            title = remoteMessage.getNotification().getTitle();
+            text = remoteMessage.getNotification().getBody();
+            id = remoteMessage.getMessageId();
+        } else {
+            title = remoteMessage.getData().get("title");
+            text = remoteMessage.getData().get("text");
+            id = remoteMessage.getData().get("id");
+
+        }
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message id: " + id);
+        Log.d(TAG, "Notification Message Title: " + title);
+        Log.d(TAG, "Notification Message Body/Text: " + text);
+
+        // TODO: Add option to developer to configure if show notification when app on foreground
+        if (!TextUtils.isEmpty(text) || !TextUtils.isEmpty(title)) {
+            sendNotification(id, title, text, remoteMessage.getData());
+        }
+    }
+
+    private void sendNotification(String id, String title, String messageBody, Map<String, String> data) {
+        Intent intent = new Intent(this, OnNotificationOpenReceiver.class);
+        Bundle bundle = new Bundle();
+        for (String key : data.keySet()) {
+            bundle.putString(key, data.get(key));
+        }
+        intent.putExtras(bundle);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, id.hashCode(), intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(getNotificationIcon())
+                .setContentTitle(title)
+                .setContentText(messageBody)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(messageBody))
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(id.hashCode(), notificationBuilder.build());
+    }
+
+    private int getNotificationIcon() {
+        int resID = getResources().getIdentifier("notification_icon", "drawable", getApplicationContext().getPackageName());
+        boolean useWhiteIcon = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP);
+        return useWhiteIcon && resID>0 ? resID: getApplicationInfo().icon;
+    }
+}
---
+++ after_prepare.js
@@ -0,0 +1,117 @@
+#!/usr/bin/env node
+'use strict';
+
+var fs = require('fs');
+
+var getValue = function(config, name) {
+    var value = config.match(new RegExp('<' + name + '>(.*?)</' + name + '>', "i"))
+    if(value && value[1]) {
+        return value[1]
+    } else {
+        return null
+    }
+}
+
+function fileExists(path) {
+  try  {
+    return fs.statSync(path).isFile();
+  }
+  catch (e) {
+    return false;
+  }
+}
+
+function directoryExists(path) {
+  try  {
+    return fs.statSync(path).isDirectory();
+  }
+  catch (e) {
+    return false;
+  }
+}
+
+var config = fs.readFileSync("config.xml").toString()
+var name = getValue(config, "name")
+
+if (directoryExists("platforms/ios")) {
+  var paths = ["GoogleService-Info.plist", "platforms/ios/www/GoogleService-Info.plist"];
+
+  for (var i = 0; i < paths.length; i++) {
+    if (fileExists(paths[i])) {
+      try {
+        var contents = fs.readFileSync(paths[i]).toString();
+        fs.writeFileSync("platforms/ios/" + name + "/Resources/GoogleService-Info.plist", contents)
+      } catch(err) {
+        process.stdout.write(err);
+      }
+
+      break;
+    }
+  }
+}
+
+if (directoryExists("platforms/android")) {
+  var paths = ["google-services.json", "platforms/android/assets/www/google-services.json"];
+
+  for (var i = 0; i < paths.length; i++) {
+    if (fileExists(paths[i])) {
+      try {
+        var contents = fs.readFileSync(paths[i]).toString();
+        fs.writeFileSync("platforms/android/google-services.json", contents);
+
+        var json = JSON.parse(contents);
+        var strings = fs.readFileSync("platforms/android/res/values/strings.xml").toString();
+
+        // strip non-default value
+        strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)</string>', "i"), '')
+
+        // strip non-default value
+        strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)</string>', "i"), '')
+
+        // strip empty lines
+        strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1')
+
+        // replace the default value
+        strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)</string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>')
+
+        // replace the default value
+        strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>')
+
+        fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
+      } catch(err) {
+        process.stdout.write(err);
+      }
+
+      break;
+    }
+  }
+
+  if (directoryExists('resources/android/notification_icon')) {
+      copyNotificationIcons();
+  }
+}
+
+function copyNotificationIcons() {
+    var filestocopy = [{
+        "resources/android/notification_icon/res/drawable-hdpi-v11/notification_icon.png": "platforms/android/res/drawable-hdpi/notification_icon.png"
+    }, {
+        "resources/android/notification_icon/res/drawable-xhdpi-v11/notification_icon.png": "platforms/android/res/drawable-xhdpi/notification_icon.png"
+    }, {
+        "resources/android/notification_icon/res/drawable-xxhdpi-v11/notification_icon.png": "platforms/android/res/drawable-xxhdpi/notification_icon.png"
+    }, {
+        "resources/android/notification_icon/res/drawable-xxxhdpi-v11/notification_icon.png": "platforms/android/res/drawable-xxxhdpi/notification_icon.png"
+    }, {
+        "resources/android/notification_icon/res/drawable-mdpi-v11/notification_icon.png": "platforms/android/res/drawable-mdpi/notification_icon.png"
+    }];
+
+    filestocopy.forEach(function(obj) {
+        Object.keys(obj).forEach(function(key) {
+            var val = obj[key];
+            var destdir = path.dirname(val);
+            if (fs.existsSync(key) && fs.existsSync(destdir)) {
+                fs.createReadStream(key).pipe(
+                    fs.createWriteStream(val));
+            }
+        });
+    });
+}
