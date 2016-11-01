6065f1051fb7d98dab38fcb513ffa288a48fe166
Noel De Martin <noeldemartin@gmail.com>
2016-11-01 10:22:48 +0100

    Add tap boolean to notification data

--- src/android/FirebasePluginMessagingService.java
+++ src/android/FirebasePluginMessagingService.java
@@ -105,6 +105,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
 
             notificationManager.notify(id.hashCode(), notificationBuilder.build());
         } else {
+            bundle.putBoolean("tap", false);
             FirebasePlugin.sendNotification(bundle);
         }
     }
