731370826c861d307d11001e0dba37f3695b9f74
FreezyExp <freezydev@live.nl>
2017-02-21 17:21:06 +0100

    escape the regex or it will cause duplicates
    adding \ resolves the double google_app_id en google_api key entries in strings.xml

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -63,19 +63,19 @@ if (directoryExists("platforms/android")) {
         var strings = fs.readFileSync("platforms/android/res/values/strings.xml").toString();
 
         // strip non-default value
-        strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)</string>', "i"), '')
+        strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)<\/string>', "i"), '')
 
         // strip non-default value
-        strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)</string>', "i"), '')
+        strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)<\/string>', "i"), '')
 
         // strip empty lines
         strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1')
 
         // replace the default value
-        strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)</string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>')
+        strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)<\/string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>')
 
         // replace the default value
-        strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>')
+        strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)<\/string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>')
 
         fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
       } catch(err) {
