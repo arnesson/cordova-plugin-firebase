80119143acac0ad94ed7be4860071b3b6fdd2aaf
Robert Arnesson <robert@robertarnesson.com>
2017-08-04 18:26:44 +0200

    android fix

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -59,25 +59,28 @@ if (directoryExists("platforms/android")) {
         var contents = fs.readFileSync(paths[i]).toString();
         fs.writeFileSync("platforms/android/google-services.json", contents);
 
-        var json = JSON.parse(contents);
-        var strings = fs.readFileSync("platforms/android/res/values/strings.xml").toString();
+        if (contents) {
+          var json = JSON.parse(contents);
 
-        // strip non-default value
-        strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)<\/string>', "i"), '')
+          var strings = fs.readFileSync("platforms/android/res/values/strings.xml").toString();
 
-        // strip non-default value
-        strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)<\/string>', "i"), '')
+          // strip non-default value
+          strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)<\/string>', "i"), '')
 
-        // strip empty lines
-        strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1')
+          // strip non-default value
+          strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)<\/string>', "i"), '')
 
-        // replace the default value
-        strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)<\/string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>')
+          // strip empty lines
+          strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1')
 
-        // replace the default value
-        strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)<\/string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>')
+          // replace the default value
+          strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)<\/string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>')
 
-        fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
+          // replace the default value
+          strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)<\/string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>')
+
+          fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
+        }
       } catch(err) {
         process.stdout.write(err);
       }
