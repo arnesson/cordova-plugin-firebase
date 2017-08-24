caac6beb5af65ad70cfa0cb71a4851ad6f022fce
Renato <rignacio@minubia.com>
2017-08-24 11:05:57 -0400

    New function: Clear all notifications
    window.FirebasePlugin.clearAllNotifications()

--- src/ios/FirebasePlugin.h
+++ src/ios/FirebasePlugin.h
@@ -23,6 +23,7 @@
 - (void)fetch:(CDVInvokedUrlCommand*)command;
 - (void)activateFetched:(CDVInvokedUrlCommand*)command;
 - (void)getValue:(CDVInvokedUrlCommand*)command;
+- (void)clearAllNotifications:(CDVInvokedUrlCommand *)command;
 @property (nonatomic, copy) NSString *notificationCallbackId;
 @property (nonatomic, copy) NSString *tokenRefreshCallbackId;
 @property (nonatomic, retain) NSMutableArray *notificationStack;
