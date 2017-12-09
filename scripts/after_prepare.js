569794c66c6b56b0c9c1432e2ecaaa8183177cd3
amritk <amrit@hockey-community.com>
2017-12-08 17:33:12 -0800

    Update after prepare for android 7.0.0

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -48,7 +48,7 @@ var PLATFORM = {
             ANDROID_DIR + '/assets/www/google-services.json',
             'www/google-services.json'
         ],
-        stringsXml: ANDROID_DIR + '/res/values/strings.xml'
+        stringsXml: ANDROID_DIR + '/app/src/main/res/values/strings.xml'
     }
 };
 
