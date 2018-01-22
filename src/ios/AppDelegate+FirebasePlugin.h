6fcd3fcd96610778fc5aaf35d040523d087b4426
Wesley Bos <wesley@digitalenterprises.nl>
2018-01-22 14:56:53 +0100

    Register as delegate and proxy calls
    - Saves reference to previous registered delegate
    - Proxies all notifications to other delegate

--- src/ios/AppDelegate+FirebasePlugin.h
+++ src/ios/AppDelegate+FirebasePlugin.h
@@ -1,5 +1,12 @@
 #import "AppDelegate.h"
 
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+@import UserNotifications;
+#endif
+
 @interface AppDelegate (FirebasePlugin)
 @property (nonatomic, strong) NSNumber *applicationInBackground;
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+@property (NS_NONATOMIC_IOSONLY, nullable, weak) id <UNUserNotificationCenterDelegate> delegate;
+#endif
 @end
