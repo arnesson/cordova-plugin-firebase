2d6b4d2d103f47c88812b7226cdb0459e334be8d
Brian Soumakian <bsoumakian@gmail.com>
2018-04-15 18:16:13 -0700

    updated test scripts

---
+++ build-android-v6.sh
@@ -0,0 +1,11 @@
+rm -rf .build-android
+
+./node_modules/.bin/cordova create .build-android com.example.hello HelloWorld
+
+cd .build-android
+
+../node_modules/.bin/cordova platform add android@~6.0.0
+
+../node_modules/.bin/cordova plugin add ..
+
+../node_modules/.bin/cordova build android
---
+++ build-ios-v4.sh
@@ -0,0 +1,11 @@
+rm -rf .build-ios
+
+./node_modules/.bin/cordova create .build-ios com.example.hello HelloWorld
+
+cd .build-ios
+
+../node_modules/.bin/cordova platform add ios@~4.0.0
+
+../node_modules/.bin/cordova plugin add ..
+
+../node_modules/.bin/cordova build ios
