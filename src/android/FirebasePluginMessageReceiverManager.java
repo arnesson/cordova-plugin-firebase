b24c8eb75216a0c24e8bf7d0012378da8f92716b
Dave Alden <dpa99c@gmail.com>
2018-04-27 10:06:03 +0100

    Replace <> operator to make code compatible with JDK 1.6

--- src/android/FirebasePluginMessageReceiverManager.java
+++ src/android/FirebasePluginMessageReceiverManager.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FirebasePluginMessageReceiverManager {
-    private static List<FirebasePluginMessageReceiver> receivers = new ArrayList<>();
+    private static List<FirebasePluginMessageReceiver> receivers = new ArrayList<FirebasePluginMessageReceiver>();
 
     public static void register(FirebasePluginMessageReceiver receiver){
         receivers.add(receiver);
