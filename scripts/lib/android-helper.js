76d46becfe979d5e699d4062dfd1fae213c65a25
Brian Bauman <brian@gigday.co>
2018-09-15 12:58:19 -0500

    Addding Crashlytics plugin dependencies to root build file along with calling it during the build

--- scripts/lib/android-helper.js
+++ scripts/lib/android-helper.js
@@ -17,14 +17,15 @@ function readRootBuildGradle() {
 /*
  * Added a dependency on 'com.google.gms' based on the position of the know 'com.android.tools.build' dependency in the build.gradle
  */
-function addGPSDependencies(buildGradle) {
+function addDependencies(buildGradle) {
   // find the known line to match
   var match = buildGradle.match(/^(\s*)classpath 'com.android.tools.build(.*)/m);
   var whitespace = match[1];
   
   // modify the line to add the necessary dependencies
-  var googlePlayDependency = whitespace + 'classpath \'com.google.gms:google-services:4.1.0\' // google-services plugin from cordova-plugin-firebase';
-  var modifiedLine = match[0] + '\n' + googlePlayDependency;
+  var googlePlayDependency = whitespace + 'classpath \'com.google.gms:google-services:4.1.0\' // google-services dependency from cordova-plugin-firebase';
+  var fabricDependency = whitespace + 'classpath \'io.fabric.tools:gradle:1.25.4\' // fabric dependency from cordova-plugin-firebase'
+  var modifiedLine = match[0] + '\n' + googlePlayDependency + '\n' + fabricDependency;
   
   // modify the actual line
   return buildGradle.replace(/^(\s*)classpath 'com.android.tools.build(.*)/m, modifiedLine);
@@ -33,14 +34,15 @@ function addGPSDependencies(buildGradle) {
 /*
  * Add 'google()' to the repository repo list
  */
-function addGoogleRepo(buildGradle) {
+function addRepos(buildGradle) {
   // find the known line to match
   var match = buildGradle.match(/^(\s*)jcenter\(\)/m);
   var whitespace = match[1];
 
   // modify the line to add the necessary repo
   var googlesMavenRepo = whitespace + 'google() // Google\'s Maven repository from cordova-plugin-firebase';
-  var modifiedLine = match[0] + '\n' + googlesMavenRepo;
+  var fabricMavenRepo = whitespace + 'maven { url \'https://maven.fabric.io/public\' } // Fabrics Maven repository from cordova-plugin-firebase'
+  var modifiedLine = match[0] + '\n' + googlesMavenRepo + '\n' + fabricMavenRepo;
 
   // modify the actual line
   return buildGradle.replace(/^(\s*)jcenter\(\)/m, modifiedLine);
@@ -65,10 +67,10 @@ module.exports = {
     var buildGradle = readRootBuildGradle();
 
     // Add Google Play Services Dependency
-    buildGradle = addGPSDependencies(buildGradle);
+    buildGradle = addDependencies(buildGradle);
   
     // Add Google's Maven Repo
-    buildGradle = addGoogleRepo(buildGradle);
+    buildGradle = addRepos(buildGradle);
 
     writeRootBuildGradle(buildGradle);
   },
