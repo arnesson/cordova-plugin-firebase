589852704646fd4e1c3864914d6dc1d022df3409
Jule Marcoueille <jule.marcoueille@gmail.com>
2018-10-24 15:02:48 +0200

    Install cordova to global in order to not mess up with npm local env.

--- test/plugin-add.sh
+++ test/plugin-add.sh
@@ -19,9 +19,9 @@ else
 fi
 
 if [[ "$PLUGIN" == "cordova-android-play-services-gradle-release" ]]; then
-  ../node_modules/.bin/cordova plugin add $PLUGIN --variable PLAY_SERVICES_VERSION=+ $FETCH_COMMAND --save
+  cordova plugin add $PLUGIN --variable PLAY_SERVICES_VERSION=+ $FETCH_COMMAND --save
 elif [[ "$PLUGIN" == "cordova-android-firebase-gradle-release" ]]; then
-  ../node_modules/.bin/cordova plugin add $PLUGIN --variable FIREBASE_VERSION=+ $FETCH_COMMAND --save
+  cordova plugin add $PLUGIN --variable FIREBASE_VERSION=+ $FETCH_COMMAND --save
 else
-  ../node_modules/.bin/cordova plugin add $PLUGIN $FETCH_COMMAND
+  cordova plugin add $PLUGIN $FETCH_COMMAND
 fi
