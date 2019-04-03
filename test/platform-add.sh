a2c57ad2154dabc491083c0a64ad592cf14369c6
Shashank Agrawal <shagrawal99@gmail.com>
2019-04-03 15:53:25 +0530

    Only installing xcode for ios platform

--- test/platform-add.sh
+++ test/platform-add.sh
@@ -9,7 +9,10 @@ PLATFORM_VERSION=$3
 FOLDER=".build-$PLATFORM"
 rm -rf $FOLDER
 
-npm install xcode --no-save
+if [[ "$PLATFORM" == "ios" ]]; then
+    npm install xcode --no-save
+fi
+
 npm install "cordova@$CORDOVA_VERSION" --no-save
 ./node_modules/.bin/cordova create $FOLDER com.github.cordova_plugin_firebase HelloWorld
 
