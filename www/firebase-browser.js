46a7bd1c06434fb4c5a72c2c20ae5d951a2e37f4
Dave Alden <dpa99c@gmail.com>
2019-05-07 10:59:41 +0100

    Fixes issues cause by Firebase SDK updates on 5 April 2019 (https://firebase.google.com/support/release-notes/android#update_-_april_05_2019) which removed deprecated API features causing Android build failures. See https://github.com/arnesson/cordova-plugin-firebase/issues/1057
    (cherry picked from commit 143f450894e89ff7d77d31197738386d5bea2a16)

--- www/firebase-browser.js
+++ www/firebase-browser.js
@@ -104,13 +104,13 @@ exports.fetch = function (cacheExpirationSeconds, success, error) {
   }
 };
 
-exports.getByteArray = function (key, namespace, success, error) {
+exports.getByteArray = function (key, success, error) {
   if (typeof success === 'function') {
     success();
   }
 };
 
-exports.getValue = function (key, namespace, success, error) {
+exports.getValue = function (key, success, error) {
   if (typeof success === 'function') {
     success();
   }
@@ -128,7 +128,7 @@ exports.setConfigSettings = function (settings, success, error) {
   }
 };
 
-exports.setDefaults = function (defaults, namespace, success, error) {
+exports.setDefaults = function (defaults, success, error) {
   if (typeof success === 'function') {
     success();
   }
