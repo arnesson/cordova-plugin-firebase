531bfe5218f45d79b67e1a9e244c60dd72c2a989
Shashank Agrawal <shagrawal99@gmail.com>
2019-07-27 01:37:43 +0530

    Removed requirement of Android hooks (which was failing at the time of plugin uninstallation) fixes #63

--- scripts/android/before_plugin_uninstall.js
+++
@@ -1,7 +0,0 @@
-var helper = require('./helper');
-
-module.exports = function(context) {
-
-    // Remove the Gradle modifications that were added when the plugin was installed.
-    helper.restoreRootBuildGradle();
-};
