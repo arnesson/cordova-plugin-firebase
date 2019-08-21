e7caa52d4c3d35645637ce33de029c35b417e00a
DL Redden <dl@wikidlabs.com>
2019-08-21 09:52:25 -0500

    Fix for short attribute in name tag
    This is the fix to issue #114

--- scripts/lib/utilities.js
+++ scripts/lib/utilities.js
@@ -24,7 +24,8 @@ module.exports = {
   getAppName: function(cb){
       var xml = fs.readFileSync("config.xml", 'utf-8');
       xml2js(xml, function(err, result){
-          cb(result.widget.name[0]);
+          var widgetName = typeof(result.widget.name[0]) === 'object' ? result.widget.name[0]._ : result.widget.name[0];
+          cb(widgetName);
       });
   },
 
