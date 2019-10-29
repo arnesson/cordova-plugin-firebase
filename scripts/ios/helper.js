ac0c0144dd791a51feb2bcd3c03e3bbbdbec302f
Dave Alden <dave@workingedge.co.uk>
2019-10-29 20:09:32 +0000

    [iOS] Automatically set up reCAPTURE verification for phone auth on iOS if SETUP_RECAPTCHA_VERIFICATION plugin variable is set

--- scripts/ios/helper.js
+++ scripts/ios/helper.js
@@ -202,6 +202,26 @@ module.exports = {
             googlePlist["FIREBASE_CRASHLYTICS_COLLECTION_ENABLED"] = (pluginVariables['FIREBASE_CRASHLYTICS_COLLECTION_ENABLED'] !== "false" ? "true" : "false") ;
             googlePlistModified = true;
         }
+        if(pluginVariables['SETUP_RECAPTCHA_VERIFICATION'] === 'true'){
+            var reversedClientId = googlePlist['REVERSED_CLIENT_ID'];
+            
+            if(!appPlist['CFBundleURLTypes']) appPlist['CFBundleURLTypes'] = [];
+            var entry, i;
+            for(i=0; i<appPlist['CFBundleURLTypes'].length; i++){
+                if(typeof appPlist['CFBundleURLTypes'][i] === 'object' && appPlist['CFBundleURLTypes'][i]['CFBundleURLSchemes']){
+                    entry = appPlist['CFBundleURLTypes'][i];
+                    break;
+                }
+            }
+            if(!entry) entry = {};
+            if(!entry['CFBundleTypeRole']) entry['CFBundleTypeRole'] = 'Editor';
+            if(!entry['CFBundleURLSchemes']) entry['CFBundleURLSchemes'] = [];
+            if(entry['CFBundleURLSchemes'].indexOf(reversedClientId) === -1){
+                entry['CFBundleURLSchemes'].push(reversedClientId)
+            }
+            appPlist['CFBundleURLTypes'][i] = entry;
+            appPlistModified = true;
+        }
 
         if(googlePlistModified) fs.writeFileSync(googlePlistPath, plist.build(googlePlist));
         if(appPlistModified) fs.writeFileSync(appPlistPath, plist.build(appPlist));
